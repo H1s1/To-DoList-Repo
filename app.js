3a56b9e28869e405c3d502f61098367f7ca1d2f8
Himanshu Soni <101991148+H1s1@users.noreply.github.com>
2023-03-12 11:17:22 +0530

    MongoDBatlas is connected

--- app.js
+++ app.js
@@ -1,56 +1,169 @@
-const bodyParser = require("body-parser");  //body-parser allows you to access req.body from within routes and use that data.
-const { urlencoded } = require("express");
 const express = require("express");
+ const bodyParser = require("body-parser");  //body-parser allows you to access req.body from within routes and use that data.
+const { urlencoded } = require("express");
+const mongoose = require('mongoose');// driver to integrate Node.js web-apps with MongoDB 
+const { redirect } = require("express/lib/response");
+const _ = require("lodash");
+const res = require("express/lib/response");
+
 
 const app = express();    //allows to set-up middleware(here post & get request is acting as a middleware) to respond to HTTP requests.
 
 const moduleDay = require(__dirname + "/date.js"); //exports desired functions and datas from the other files 
                           // here functions of date.js file is getting exorted to app.js file
-var items = [];
-var workItems = [];
+                          
+                          var workItems = [];
+                          
+                          
+                          
+ app.use(express.urlencoded({extended:true})); //It parses the data coming from post request .                     
+ app.use(express.static(__dirname )); // Used to serve static files such as images, CSS files, and JavaScript files.
+ app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs');
+// Mongoose or DataBase Section :- 
 
-app.use(express.urlencoded({extended:true})); //It parses the data coming from post request .
 
-app.use(express.static(__dirname )); // Used to serve static files such as images, CSS files, and JavaScript files.
+main().catch(err => console.log(err));
 
-app.get("/",function(req,res){
+async function main(){
+
+    // Establishing connection between DB and this webApp.
+    mongoose.connect("mongodb+srv://Himanshu:Test1234@cluster0.y4nquy6.mongodb.net/todolistDB");
     
-    let day = moduleDay.getTime();
-     
-    res.render("list", {listTitle: day , newListItems: items}); // Rendering is the process of gathering data (if any)
-     //Then applying the gathered data to the associated templates(here our associated template is list.ejs) and then the final output is sent to the user. 
-});                                                               
+    //Schema or bluePrint of records in document
+    const itemsSchema = new mongoose.Schema({
+        Name: String
+       //, items : []
+    });
 
-app.post('/',(req,res)=>{
-    var item = req.body.newItem;
-     //console.log(req.body);
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work')
-    }else{
-        items.push(item);
-        res.redirect('/');//The res. redirect() function lets you redirect the user to a different URL by sending an HTTP response with status 302. 
-        //The HTTP client  will then "follow" the redirect and send an HTTP request to the new URL 
+    //Model(An isnstance of model is called a document)
+    const Item = mongoose.model("Item" , itemsSchema);
+
+    //Default items in the list
+    const Item1 = new Item ({
+        Name:"do exercise"
+    });
+    const Item2 = new Item ({
+        Name:"do learingng"
+    });
+    const Item3 = new Item ({
+        Name:"eat goodFoods"
+    });
+ 
+    const defaultitmes = [ Item1 ,Item2 , Item3];
+
+    const listSchema = {
+        Name:String,
+        items: [itemsSchema]
     }
-  
-});
 
-app.get("/work",(req,res)=>{
-      res.render('list', {listTitle: "Work List" , newListItems: workItems});
-});
+    const List = mongoose.model("List", listSchema);
+    
+   const day = moduleDay.getTime();
+   
+//........... Get root route ...........
+      app.get("/",function(req,res){
+        
+       Item.find({},function(err,foundItems) {
+   
+           if(foundItems.length === 0){
+            Item.insertMany(defaultitmes , function(err){
+               if(err){
+                   console.log(err);
+               }else{
+                   console.log("successfully saved default items to DB.");
+               }
+               res.redirect("/");
+            });
+           }
+           else
+           { 
+             res.render("list" , {listTitle : day , newListItems : foundItems });
+           }
+          });
+       }); 
+       
+//.........Adding new items to the list or DB........     
+            app.post('/',(req,res)=>{
+                const itemName = req.body.newItem;//new item you inserted in new/old list
+                const listName = req.body.list; // title of the new/old list
+       
+                 const item = new Item({
+                    Name: itemName
+                 });
+                
+       
+                 if(listName === day){
+                   item.save();
+                   res.redirect("/");
+                 }
+                  else 
+                 {
+                  List.findOne({Name:listName},function(err, foundList){
+                    console.log( "This is foundList.items :- " + foundList.items);
+                    foundList.items.push(item);
+                    foundList.save();
+                    res.redirect("/" + listName);
+                  });
+                 }
+            
+            });
+   
+   //dynamic routing with the help of express.js
+   app.get("/:coustomListName", function(req,res){
+     const coustomListName = _.capitalize(req.params.coustomListName);
+      console.log(coustomListName);
+      if(coustomListName === "Favicon.ico") return;
+      console.log(coustomListName + " line no. 131 ");
 
-app.post("/work",function(req,res){
- let item = req.body.newItem;
- workItems.push(item);
- res.redirect("/work");
-});
 
-app.get("/about",function(req,res){
- res.render('about');
-});
+     List.findOne({Name:coustomListName} , function(err,foundList){
+       if(!err)
+       {
+         if (!foundList){
+           //...........Create a new list on DB........
+           const list = new List({
+             Name: coustomListName ,
+             items : defaultitmes
+           });
+           list.save();
+           console.log("HEy folk! I am here on line no.->92 ");
+           res.render("/" + coustomListName );
+         } 
+         else{
+           //..............Show up an existing list................
+           res.render("list.ejs", {listTitle: foundList.Name , newListItems: foundList.items});
+         }
+       }
+     });
+    });
 
-app.listen(3000,function(){
-    console.log("port started at local host 3000");
-});
+   
+  //......... Delteing check elemets from DB......
+    app.post('/delete',function(req,res){
+      
+     const checkedItemid = req.body.checkbox;
+     const listName = req.body.listName;
+     
+     if (listName === day)
+      {
+       Item.findByIdAndRemove(checkedItemid,function(err){
+         if(!err){console.log("deleted successfully !!!");}
+         
+         res.redirect('/');
+       });
+     }
+     else{
+      List.findOneAndUpdate({Name: listName},{$pull :{items: {_id : checkedItemid}}},(err,foundList)=>{
+        if(!err){
+          res.redirect("/"+ listName);
+        }
+      })
+     }
+    });
+
+}
+
+app.listen(80,function(){
+    console.log("port started at local host 80");
+}); 
